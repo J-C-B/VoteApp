338b866739ac5c95d16967c80d1ace6c485e611f
John <J-C-B@users.noreply.github.com>
2024-09-24 13:49:23 +1200

    Update vpc.tf

--- terraform/aws/jenkins/vpc.tf
+++ terraform/aws/jenkins/vpc.tf
@@ -60,6 +60,27 @@ resource "aws_security_group" "ingress-ssh-from-all" {
   }
 }
 
+resource "aws_security_group" "ingress-ssh-add-more-bad" {
+  name   = "JB-allow-all-ssh-sg"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "ingress-port-8080-from-alb" {
   name   = "jenkins-allow-ui-traffic"
   vpc_id = aws_vpc.vpc.id
